54d484d325e9eceaaeabc2615aab6f109d7e3beb
ajnel221 <ajnel221@gmail.com>
2021-03-02 18:51:27 +0200

    Added Camera Collision Script

---
+++ BasicSkilltreeSystem/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj;
+    Vector3 followPos;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotX = 0.0f;
+    private float rotY = 0.0f;
+
+    private void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse Y");
+        mouseY = Input.GetAxis("Mouse X");
+        finalInputX = mouseX;
+        finalInputZ = mouseY;
+
+        rotX += -finalInputX * inputSensitivity * Time.deltaTime;
+        rotY += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);    
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    private void LateUpdate()
+    {
+        CameraUpdater();    
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = cameraFollowObj.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
